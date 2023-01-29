d472b0e37be613e491e2b80a164fc4c6bb7bf9fa
Fatima Taoufiq <92335403+Fatimatq@users.noreply.github.com>
2023-01-29 22:47:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet,  TouchableWithoutFeedback, View, Keyboard } from 'react-native';
 import AddAlergy from './component/addAlergy';
 import NavBar from './component/nav';
