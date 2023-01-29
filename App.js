54fded56262d9c8cadb99a1b0dbdcc6326750440
abirhsaini <abirhsaini@gmail.com>
2023-01-29 17:17:45 +0100

    fixer les folders et app.js pour les screens

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
-    return ( <
-        View style = { styles.container } >
-        <
-        Text > Open up App.js to start working on your app! < /Text> <
-        StatusBar style = "auto" / >
-        <
-        /View>
+  return (
+    <NavigationContainer>
+   <Stack.Navigator>
+   <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+</Stack.Navigator>
+    </NavigationContainer>
+ 
 
-    );
+
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 2,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
