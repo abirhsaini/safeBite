a9c623b5b62e32edfbc9cfe2614d735adcd3413f
abirhsaini <abirhsaini@gmail.com>
2023-01-30 16:00:39 +0100

    interface de login

--- App.js
+++ App.js
@@ -8,12 +8,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-   <Stack.Navigator>
-   <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator>
+        <Stack.Screen name="Login"
+          component={Login}
+          options={{headerShown:false}}  />
         <Stack.Screen name="Home" component={Home} />
-</Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
- 
+
 
 
   );
