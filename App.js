81d4fb3a83ee11f9a1eb1b51628f1bf8dfc985d4
bachikhsoukaina <bachikhsoukaina@gmail.com>
2023-01-30 15:21:28 +0100

    modify BarscodeScanner screen

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import Home from './pages/Home';
 import Login from './pages/Login';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {BarScannerComponent} from './pages/BarcodeScanner.jsx'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BarScannerComponent from './pages/BarcodeScanner'
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 export default function App() {
   return (
+    <BarScannerComponent/>
 //     <NavigationContainer>
 //    <Stack.Navigator>
 //    <Stack.Screen name="Login" component={Login} />
 //         <Stack.Screen name="Home" component={Home} />
 //     </Stack.Navigator>
 //     </NavigationContainer>
-    <BarScannerComponent/>
  
 
 
--- pages/BarcodeScanner.jsx
+++ pages/BarcodeScanner.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import {StatusBar} from 'expo-status-bar';
-import {BarCodeScanner} from "expo-barcode-scanner";
-import { Button, View, Text, StyleSheet } from "react-native";
-// import BarcodeScanner from 'react-native-barcodescanner';
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Button, View, Text, StyleSheet, Image } from "react-native";
 
 
-export default BarScannerComponent = () =>{
+export default function BarScannerComponent(){
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
     const [text, setText] = useState("Not yet scanned!");
+    const [product_name, setProduct_name] = useState("");
+    const [product_image, setProduct_image] = useState("")
 
-    const askForCameraPermission = ()=>{
+    const askForCameraPermission = () =>{
         (async ()=>{
-            const {status} = await BarCodeScanner.requestPermissionAsync();
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status == 'granted')
         })()
     }
@@ -22,12 +23,31 @@ export default BarScannerComponent = () =>{
         askForCameraPermission();
     },[]);
 
+    //fetching data from open data foods
+    useEffect(()=>{
+        if(scanned===true){
+            fetch(`https://world.openfoodfacts.org/api/v2/search?code=${text}&fields=code,product_name,image_front_small_url`)
+            .then(response=>response.json())
+            .then(response=>{
+                let product_name = response.products[0].product_name
+                let product_mage = response.products[0].product_image
+                console.log(response.products)
+                if(response.products===[]){
+                    return setProduct_name("Desole le produit n'existe pasactuellement")
+                }
+                else{return setProduct_name(product_name), setProduct_image(product_image)}
+            })
+            .catch(error=>console.log(error))
+        }
+    })
+
     //What happens when we scan the bar code
-    const handleBarCodeScanned = ({type,data})=>{
+    const handleBarCodeScanned = ({type, data}) => {
         setScanned(true);
         setText(data);
         console.log(`type: ${type} 
-        Data: ${data}`)
+                    Data: ${data}
+                    product_name: ${product_name}`)
     }
 
     //Check for permissions and return the screens
@@ -53,8 +73,24 @@ export default BarScannerComponent = () =>{
     return(
         <View style={styles.container}>
             <View style={styles.barcodebox}>
-
+                <BarCodeScanner
+                showMarker={true}
+                torchMode={'on'}
+                onBarCodeScanned={scanned?undefined:handleBarCodeScanned}
+                style={{height:600, width:400}}
+                />
+                
             </View>
+            <Text style={styles.maintext}>{text}</Text>
+            <Image
+                source={{uri: product_image}}
+                defaultSource="https://images.openfoodfacts.org/images/products/611/124/210/1197/front_fr.26.200.jpg"
+                // style={{ width: 200, height: 200 }}
+            />
+
+            <Text style={styles.product_name}> bla bla {product_name}</Text>
+            {scanned && <Button style={{position:'absolue', bottom:2}} title={'scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+            
         </View>
     );
 }
@@ -62,18 +98,29 @@ export default BarScannerComponent = () =>{
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        backgroundColor:'#fff',
+        backgroundColor:'#FFF8E7',
         alignItems:'center',
         justifyContent:'center'
     },
     barcodebox:{
+        // position: 'absolute',
+        // top:100,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
         height: 300,
-        width: 300,
+        width: 340,
         overflow: 'hidden',
-        borderRadius: 30,
-        backgroundColor: 'tomato'
+        borderRadius: 20,
+        marginTop:0
+    },
+    maintext:{
+        fontSize:16,
+        margin:20,
+    },
+    product_name:{
+        fontSize:16,
+        margin:20,
+        textColor:'red'
     }
 });
